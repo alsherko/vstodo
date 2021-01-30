478a126ec440718b40ae01f84f07f16ae0b2fb80
alsherko <al.sherko@gmail.com>
2021-01-30 17:21:54 +0300

    custom command

--- src/extension.ts
+++ src/extension.ts
@@ -6,22 +6,26 @@ import * as vscode from 'vscode';
 // your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
 
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
 	console.log('Congratulations, your extension "vstodo" is now active!');
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('vstodo.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-
-		// Display a message box to the user
+	context.subscriptions.push(vscode.commands.registerCommand('vstodo.helloWorld', () => {
 		vscode.window.showInformationMessage('Hello World from VSTodo!');
-	});
+	}))
 
-	context.subscriptions.push(disposable);
+	context.subscriptions.push(vscode.commands.registerCommand("vstodo.askQuastion", async () => {
+		const answer = await vscode.window.showInformationMessage(
+			"How was your day?",
+			"good",
+			"bad"
+		);
+
+		if (answer === 'bad') {
+			vscode.window.showInformationMessage("Sorry to hear that")
+		} else {
+			console.log(answer)
+		}
+	}))
 }
 
 // this method is called when your extension is deactivated
-export function deactivate() {}
+export function deactivate() { }
