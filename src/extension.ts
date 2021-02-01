37950b9635fdccb087201378dbba6ae148a8cb6f
alsherko <al.sherko@gmail.com>
2021-02-01 09:26:03 +0300

    sidebar

--- src/extension.ts
+++ src/extension.ts
@@ -2,21 +2,32 @@
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
 import { HelloWorldPanel } from './HelloWordPanel';
+import { SidebarProvider } from './SidebarProvider';
 
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
 
-	console.log('Congratulations, your extension "vstodo" is now active!');
+	const sidebarProvider = new SidebarProvider(context.extensionUri);
+	context.subscriptions.push(
+	  vscode.window.registerWebviewViewProvider(
+		"vstodo-sidebar",
+		sidebarProvider
+	  )
+	);
 
 	context.subscriptions.push(vscode.commands.registerCommand('vstodo.helloWorld', () => {
 		console.log(context.extensionUri)
 		HelloWorldPanel.createOrShow(context.extensionUri)
 	}))
 
-	context.subscriptions.push(vscode.commands.registerCommand('vstodo.refresh', () => {
-		HelloWorldPanel.kill()
-		HelloWorldPanel.createOrShow(context.extensionUri)
+	context.subscriptions.push(vscode.commands.registerCommand('vstodo.refresh', async () => {
+		// HelloWorldPanel.kill()
+		// HelloWorldPanel.createOrShow(context.extensionUri)
+
+		await vscode.commands.executeCommand("workbench.action.closeSidebar")
+		await vscode.commands.executeCommand("workbench.view.extension.vstodo-sidebar-view")
+
 		setTimeout(() => { 
 			vscode.commands.executeCommand(
 				'workbench.action.webview.openDeveloperTools'
