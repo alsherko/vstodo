ad810e14180b147430c2eb96fe0c711a8e7380d2
alsherko <al.sherko@gmail.com>
2021-02-09 14:58:17 +0300

    connect postgres

--- api/src/index.ts
+++ api/src/index.ts
@@ -1,12 +1,33 @@
 import express from 'express';
+import { join } from 'path';
+import {createConnection} from 'typeorm'
+import { __prod__ } from './constants';
+import {User} from "./entities/User";
+
+require('dotenv').config();
 
 const main = async () => {
+    await createConnection({
+        type: 'postgres',
+        database: process.env.POSTGRES_DATABASE,
+        username: process.env.POSTGRES_USERNAME,
+        password: process.env.POSTGRES_PASSWORD,
+        entities: [join(__dirname, './entities/*.*')],
+        logging: !__prod__,
+        synchronize: !__prod__,
+
+    })
+    
+    const user = await User.create({name: 'bob'}).save();
+
+    console.log({user})
+
     const app = express()
     app.get('/', (_req, res) => {
         res.send("hello");
     })
-    app.listen(3002, () => {
-        console.log('listerning on localhost:3002')
+    app.listen(3003, () => {
+        console.log('listening on localhost:3002')
     })
 };
 
