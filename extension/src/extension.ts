221f862aaf98765f7781755f174172177580bbd0
alsherko <al.sherko@gmail.com>
2021-02-10 10:30:12 +0300

    vscode save token

--- extension/src/extension.ts
+++ extension/src/extension.ts
@@ -1,12 +1,15 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
+import { authenticate } from './authenticate';
 import { HelloWorldPanel } from './HelloWordPanel';
 import { SidebarProvider } from './SidebarProvider';
+import { TokenManager } from './TokenManager';
 
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
+	TokenManager.globalState = context.globalState;
 
 	const sidebarProvider = new SidebarProvider(context.extensionUri);
 
@@ -23,10 +26,23 @@ export function activate(context: vscode.ExtensionContext) {
 	);
 
 	context.subscriptions.push(vscode.commands.registerCommand('vstodo.helloWorld', () => {
-		console.log(context.extensionUri)
-		HelloWorldPanel.createOrShow(context.extensionUri)
+		// console.log(context.extensionUri)
+		// HelloWorldPanel.createOrShow(context.extensionUri)
+		vscode.window.showInformationMessage(
+			"token value is: " + TokenManager.getToken()
+		)
 	}))
 
+	context.subscriptions.push(vscode.commands.registerCommand('vstodo.authenticate', () => {
+		try{
+			authenticate();
+		} catch(err){
+			console.log(err)
+		}
+
+		
+	}))
+	
 	context.subscriptions.push(vscode.commands.registerCommand('vstodo.addTodo', () => {
 		const {activeTextEditor} = vscode.window
 
