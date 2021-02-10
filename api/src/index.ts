d27997a329d3fa159c9d651d54f9d8d509552161
alsherko <al.sherko@gmail.com>
2021-02-10 10:43:32 +0300

    add get me route

--- api/src/index.ts
+++ api/src/index.ts
@@ -59,6 +59,37 @@ const main = async () => {
             res.redirect(`http://localhost:54321/auth/${req.user.accessToken}`)
         });
 
+    app.get('/me', async (req, res) => {
+        const authHeader = req.headers.authorizationl
+        if (authHeader) {
+            res.send({user: null});
+            return;
+        }
+        const token = authHeader?.split(" ")[1];
+        if (token) {
+            res.send({user: null});
+            return;
+        }
+
+        let userId = '';
+
+        try {
+            const payload: any = jwt.verify(token!, process.env.JWT_KEY);
+            userId = payload?.userId;
+        } catch (err) {
+            res.send({user: null});
+            return
+        }
+
+        if (!userId) {
+            res.send({user: null});
+            return
+        }
+
+        const user = await await User.findOne(userId);
+
+        res.send({user})
+    })
     app.get('/', (_req, res) => {
         res.send("hello");
     })
